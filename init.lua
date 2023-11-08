00d41d8d05dd8662d04da47eaa00603cd5d23805
eyronjuude <actanael@up.edu.ph>
2023-11-08 08:36:41 +0800

    chore: relocate `vim-plug` to `lua/plugins`

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require "plugins"
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.autoindent = true
@@ -8,30 +10,6 @@ vim.opt.shiftwidth = 4
 vim.opt.encoding = 'utf-8'
 
 vim.o.guifont = 'JetBrains Mono:h11'
-
-vim.call('plug#begin', '~/.config/nvim/plugged')
-
-local Plug = vim.fn['plug#']
-Plug('https://github.com/tpope/vim-surround') -- Surrounding ysw
-Plug('https://github.com/preservim/nerdtree') -- NerdTree
-Plug('https://github.com/tpope/vim-commentary') -- For Commenting gcc & gc
-Plug('https://github.com/vim-airline/vim-airline') -- Status bar
-Plug('https://github.com/vim-airline/vim-airline-themes') -- Themes
-Plug('https://github.com/lifepillar/pgsql.vim') -- PSQL Pluging needs :SQLSetType pgsql.vim
-Plug('https://github.com/ap/vim-css-color') -- CSS Color Preview
-Plug('https://github.com/rafi/awesome-vim-colorschemes') -- Retro Scheme
-Plug('https://github.com/neoclide/coc.nvim', { ['branch'] = 'release' })  -- Auto Completion
-Plug('https://github.com/ryanoasis/vim-devicons') -- Developer Icons
-Plug('https://github.com/tc50cal/vim-terminal') -- Vim Terminal
-Plug('https://github.com/preservim/tagbar') -- Tagbar for code navigation
-Plug('https://github.com/terryma/vim-multiple-cursors') -- CTRL + N for multiple cursors
-Plug('https://github.com/williamboman/mason.nvim') -- Portable Package Manager for LSPs
-Plug('https://github.com/williamboman/mason-lspconfig.nvim') -- Mason LSP Configuration
-Plug('https://github.com/neovim/nvim-lspconfig') -- Neovim LSP Configuration
-Plug('https://github.com/nvim-tree/nvim-web-devicons') -- Recommended (for coloured icons)
-Plug('https://github.com/akinsho/bufferline.nvim', { ['tag'] = '*' })
-
-vim.call('plug#end')
 
 vim.cmd[[colorscheme onedark]]
 
